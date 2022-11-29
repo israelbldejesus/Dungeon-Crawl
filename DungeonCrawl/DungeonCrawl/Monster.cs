57e994fbf739ca50902d647d0c6a9d154e3f7103
vivnwin <vivi.nguyen5628@gmail.com>
2022-11-29 09:01:42 -0600

    cleaned up monster.cs

--- DungeonCrawl/DungeonCrawl/Monster.cs
+++ DungeonCrawl/DungeonCrawl/Monster.cs
@@ -7,33 +7,26 @@ namespace DungeonCrawl
 {
     public class Monster
     {
-        /// <summary>
+
         /// This is the health of the monster.
-        /// </summary>
         public int HP;
-        /// <summary>
+
         /// This will determine the base HP and Attack.
-        /// </summary>
         public int Level;
-        /// <summary>
+
         /// This is how much damage the monster does
-        /// </summary>
         public int Attack;
-        /// <summary>
+
         /// This is the factor that will determine chance of blocking attack.
-        /// </summary>
         public int Armor;
-        /// <summary>
+
         /// This is the stat that will determine chance of hitting.
-        /// </summary>
         public int Dexterity;
-        /// <summary>
+
         /// This determines if a monster is a boss.
-        /// </summary>
         public bool Boss;
-        /// <summary>
+
         /// This list will contain all the types of monsters.
-        /// </summary>
         public List<Monster> MonsterType;
 
         public Room Room
