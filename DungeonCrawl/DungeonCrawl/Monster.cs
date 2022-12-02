efc7591ccaf90c557bb8479b463589eb2ec5976a
vivnwin <vivi.nguyen5628@gmail.com>
2022-12-01 19:51:42 -0600

    attempted to add external file in monster

--- DungeonCrawl/DungeonCrawl/Monster.cs
+++ DungeonCrawl/DungeonCrawl/Monster.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace DungeonCrawl
 {
@@ -33,6 +34,39 @@ namespace DungeonCrawl
 
         /// This is describing the monster.
         public string Description;
+
+
+        //static void Main(string[] args)
+        //{
+        //    // inserting external file and adding monsters to file
+        //    string path = "Monster.txt"; // file for monster
+        //    string excerpt = null;
+
+        //    if (File.Exists(path)) // if file exist, execute if code
+        //    {
+        //        using (StreamReader sr = new StreamReader(path))
+        //        {
+        //            while (!sr.EndOfStream)
+        //            {
+        //                excerpt = sr.ReadLine();
+        //                Console.WriteLine(excerpt);
+        //            }
+
+        //            sr.Close();
+        //            Pause();
+        //        }
+        //        using (StreamReader sr2 = new StreamReader(path))
+        //        {
+        //            while (!sr2.EndOfStream)
+        //            {
+        //                Monster MonsterType = new Monster();
+        //            }
+
+        //            sr2.Close();
+        //        }
+        //    }
+        //}
+                    
 
         public Room Room
         {
