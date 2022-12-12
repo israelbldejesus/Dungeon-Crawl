35204924df52fe8fc0ac28f24e02a43f89b3b9bf
israelbldejesus@gmail.com <israelbldejesus@gmail.com>
2022-12-12 16:26:26 -0600

    added more testing

--- TestProbability/TestProbability/Program.cs
+++ TestProbability/TestProbability/Program.cs
@@ -1,19 +1,28 @@
 ﻿bool keepLoop = true;
 do
 {
-    Random rand = new Random();
-    int yes = 0;
-    const int iterations = 10000000;
-    for (int i = 0; i < iterations; i++)
-    {
-        if (rand.Next(1, 101) <= 25)
-        {
-            yes++;
-        }
-    }
-    Console.WriteLine((float)yes / iterations);
-    Console.WriteLine("Want to keep looping?(y/n)");
-    if(Console.ReadLine() == "n")
+    /* Random rand = new Random();
+     int yes = 0;
+     const int iterations = 10000000;
+     for (int i = 0; i < iterations; i++)
+     {
+         if (rand.Next(1, 101) <= 25)
+         {
+             yes++;
+         }
+     }
+     Console.WriteLine((float)yes / iterations);
+     Console.WriteLine("Want to keep looping?(y/n)");
+    }*/
+    int pDex = 8;
+
+    int output = pDex * 10;
+    //output /= 20;
+
+    Console.WriteLine(output);
+
+    //return output;
+    if (Console.ReadLine() == "n")
     {
         keepLoop = false;
     }
